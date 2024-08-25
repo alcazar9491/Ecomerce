fdd92dc3e16fbe904bd40432332ec74368db6662
Jorge Alcazar Diaz <80380017+alcazar9491@users.noreply.github.com>
2024-08-25 00:03:58 -0700

    Initial Commit

---
+++ Ecomerce/EcomerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomerceApp.swift
+//  Ecomerce
+//
+//  Created by jorge alcazar on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EcomerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
